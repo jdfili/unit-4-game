297837ece803df5075a19bad07f9497ebffc75b6
JD <jdfili14@gmail.com>
2018-11-10 22:17:12 -0800

    outline

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    var counter = 0;
+    var targetNumber = 50;
+    var wins = 0;
+    var losses = 0;
+    var numberOptions = [10, 3, 5, 7];
+    targetNumber = 43;
+
+    for (var i = 0; i < numberOptions.length; i++) {
+        var imageCrystal = $("<img>");
+        imageCrystal.addClass("crystal-image");
+        imageCrystal.attr("src", "assets/images/mirage crystal--010.jpg");
+        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+        $("#crystals").append(imageCrystal);
+    }
+
+    $(".number-guess").text("Number to guess: " + targetNumber);
+
+    $(".crystal-image").on("click", function () {
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        counter += crystalValue
+        $(".user-number").text("Your score: " + counter);
+        if (targetNumber === counter) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            counter = 0;
+            $(".user-number").text("Your score: " + counter)
+        }
+        else if (counter > targetNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            counter = 0;
+            $(".user-number").text("Your score: " + counter)
+        }
+
+
+
+
+
+    })
+
+    // $(".number-guess").text("Number to guess: " + targetNumber);
+    // $("#wins").text("Wins: " + wins);
+    // $("#losses").text("Losses: " + losses);
+
+    // $(".crystal-image").on("click", function () {
+    //     counter += 10;
+    //     $(".user-number").text("Your score: " + counter);
+    //     if (targetNumber === counter) {
+    //         wins++;
+    //         $("#wins").text("Wins: " + wins);
+    //         counter = 0;
+    //         $(".user-number").text("Your score: " + counter);
+    //     }
+
+    // })
+})
+

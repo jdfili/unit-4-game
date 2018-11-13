0a79797dbb0cdcb0cf664309e8791f45d868498b
JD <jdfili14@gmail.com>
2018-11-12 22:10:55 -0800

    fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,45 +3,63 @@ $(document).ready(function () {
     var counter = 0;
     var wins = 0;
     var losses = 0;
-    var targetNumber = Math.floor(Math.random() * 50) + 30;
+    var targetNumber = Math.floor(Math.random() * 30) + 30;
 
-    for (var i = 1; i < 5; i++) {
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystal-image");
-        imageCrystal.attr("src", "assets/images/mirage crystal--010.jpg");
-        imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * 10) + 1);
-        $("#crystals").append(imageCrystal);
+    function images() {
+        for (var i = 1; i < 5; i++) {
+            var imageCrystal = $("<img>");
+            imageCrystal.addClass("crystal-image");
+            imageCrystal.attr("src", "assets/images/mirage crystal--010.jpg");
+            imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * 9) + 2);
+            $("#crystals").append(imageCrystal);
+        }
+    };
+
+    function html() {
+        $(".number-guess").text("Number to guess: " + targetNumber);
+        $(".user-number").text("Your score: " + counter);
+        $("#wins").text("Wins: " + wins);
+        $(".losses").text("Losses: " + losses);
+        $("#crystals").empty();
+
+    };
+
+    function reset() {
+        counter = 0;
+        targetNumber = Math.floor(Math.random() * 30) + 30;
+        html();
+        images();
+        
     }
+    // ------------------------------------------------------------------------
+    html();
+    images();
+    
 
-    $(".number-guess").text("Number to guess: " + targetNumber);
-
-    $(".crystal-image").on("click", function () {
+    $("#crystals").on("click", "img", function () {
         var crystalValue = ($(this).attr("data-crystalvalue"));
         crystalValue = parseInt(crystalValue);
-        counter += crystalValue
+        counter += crystalValue;
         $(".user-number").text("Your score: " + counter);
         if (targetNumber === counter) {
             wins++;
             $("#wins").text("Wins: " + wins);
-            counter = 0;
-            $(".user-number").text("Your score: " + counter); 
-            targetNumber = Math.floor(Math.random() * 50) + 30;
-            $(".number-guess").text("Number to guess: " + targetNumber);   
+            reset();
         }
-        else if (counter > targetNumber) {
+
+        if (targetNumber < counter) {
             losses++;
             $("#losses").text("Losses: " + losses);
-            counter = 0;
-            $(".user-number").text("Your score: " + counter);
-            targetNumber = Math.floor(Math.random() * 50) + 30;
-            $(".number-guess").text("Number to guess: " + targetNumber);  
-           
+            reset();
         }
-
-
-
-
-
     })
+
+
+
+
+
+
+
+
 
 })

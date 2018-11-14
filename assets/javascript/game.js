94b7ad1b45ce240805c5cfd802305728206f7c8c
JD <jdfili14@gmail.com>
2018-11-13 21:35:29 -0800

    all done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
     var wins = 0;
     var losses = 0;
 
+
     function newTargetNumber() {
-        targetNumber = Math.floor(Math.random() * 120) + 19;
+        targetNumber = Math.floor(Math.random() * 101) + 19;
     };
 
     function images() {
